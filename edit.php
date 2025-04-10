ef155aede8ea3840faa59c6ab4d25dcab7b14d31
gohel ramji <gohelramjee3230@gmail.com>
2025-04-10 11:37:06 +0530

    Refactor code for improved readability and consistency in product forms

--- edit.php
+++ edit.php
@@ -1,12 +1,9 @@
 <?php
 //include database connection file
 include "dbconnect.php";
-
 //get product id bty url
 $id = $_GET["id"];
-
 //check form submitted
-
 if (isset($_POST["submit"])) {
     //resive data form post method
     //and store in variable
@@ -19,7 +16,6 @@ if (isset($_POST["submit"])) {
     if ($product_name == "") {
         $errors[] = "Please Enter Product Name";
     }
-
     elseif(!preg_match("/^[a-zA-Z]*$/", $product_name)) {
         $errors[] = "Product Name must be letters only";
     }
@@ -30,7 +26,6 @@ if (isset($_POST["submit"])) {
         $errors[] = "Price Enter must be a number between 0 and 100000";
     } elseif ($price < 0) {
         $errors[] = "Price must be greater than 0";
-
     } elseif ($price > 100000) {
         $errors[] = "Price must be less than 100000";
     }
@@ -52,9 +47,7 @@ if (isset($_POST["submit"])) {
     if (!preg_match("/^[a-zA-Z ,]*$/", $category)) {
         $errors[] = "Category must be letters only";
     }
-
     if (empty($errors)) {
-
         //chech if product name alreaddy exists in database 
         $check_sql = "SELECT * FROM `product_list` WHERE product_name='$product_name' AND id != $id";
         $check_sql_result = mysqli_query($conn, $check_sql);
@@ -78,9 +71,7 @@ if (isset($_POST["submit"])) {
         }
     }
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -94,9 +85,7 @@ if (isset($_POST["submit"])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
     <title>PHP CRUD Application</title>
-
 </head>
 
 <body>
@@ -104,7 +93,6 @@ if (isset($_POST["submit"])) {
     <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color:rgba(32, 193, 218, 0.45);">
         PHP Product Application - Edit Product
     </nav>
-
     <div class="container">
         <div class="text-center mb-4">
             <h3>Edit Product Details</h3>
@@ -161,9 +149,7 @@ if (isset($_POST["submit"])) {
         </div>
     </div>
     <!-- Bootstrap -->
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 // include  the database connection file
+
 include "dbconnect.php"; ?>
 
 <!DOCTYPE html>
@@ -11,6 +12,7 @@ include "dbconnect.php"; ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap  csss for styling-->
     <link rel="stylesheet" href="style.css">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Font Awesome for icon -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -45,6 +47,7 @@ include "dbconnect.php"; ?>
                         <th scope="col">Category</th>
                         <th scope="col">Action</th>
                     </tr>
+
                 </thead>
                 <tbody>
                     <?php
